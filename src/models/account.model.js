d58a1540730226b61904aab92a33cc6fff69373e
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-04-04 18:22:26 +0530

    model added

---
+++ src/models/account.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:[true, "Account must be associate with a user"],
+        index:true
+   },
+   status:{
+    type:String,
+    enum:{
+        values:["ACTIVE", "FROZEN", "CLOSED"],
+        message:"Status can be either ACTIVE, FROZEN or CLOSED"
+    }
+   },
+   currency:{
+    type:String,
+    required:[true, "Currency is required"],
+    default:"INR"
+   }
+},{
+    timestamps:true
+})
+
+accountSchema.index({user:1, status:1})
+
+const Account = mongoose.model("Account", accountSchema)
+
+module.exports = accountSchema
