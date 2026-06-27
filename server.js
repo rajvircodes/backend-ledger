9fc73b753f07223500fbebef13428652198b0529
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-28 01:58:37 +0530

    refactor(server): improve application startup flow

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const app = require("./src/app")
-const connectToDB = require("./src/config/db")
+const app = require("./src/app");
+const connectToDB = require("./src/config/db");
 
-connectToDB()
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+const startServer = async () => {
+  try {
+    await connectToDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port : ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
--- src/app.js
+++ src/app.js
@@ -1,31 +1,29 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
+const app = express();
 
-
-const app = express()
-
-
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 /**
  * - Routes required
  */
-const authRouter = require("./routes/auth.routes")
-const accountRouter = require("./routes/account.routes")
-const transactionRoutes = require("./routes/transaction.routes")
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+const transactionRoutes = require("./routes/transaction.routes");
 
 /**
  * - Use Routes
  */
 
 app.get("/", (req, res) => {
-    res.send("Ledger Service is up and running")
-})
+  res.send("Ledger Service is up and running");
+});
 
-app.use("/api/auth", authRouter)
-app.use("/api/accounts", accountRouter)
-app.use("/api/transactions", transactionRoutes)
+app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+app.use("/api/transactions", transactionRoutes);
 
-module.exports = app
+module.exports = app;
